1f9a08194c5767f6438cde0b4112da8b3aa62c1d
Adam Mauch <maucap20@wfu.edu>
2023-08-09 10:25:28 -0400

    starter home page

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Main() {
-
-    return (
-        <div>Welcome to the homepage</div>
-    )
+  return (
+    <div className="main-container">
+      <h1>Welcome to Our Website!</h1>
+      <p>
+        We're excited to have you here!
+      </p>
+      <p>
+        Feel free to explore the different sections of our site using the links below:
+      </p>
+      <ul>
+        <li>
+          <Link to="/movies">View Movie Catalog</Link>
+        </li>
+        <li>
+          <Link to="/users">Meet Our Users</Link>
+        </li>
+        <li>
+          <Link to={`/profile/${userId}`}>Your Profile</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Main;
